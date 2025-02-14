5bb50e79d8241b72ff34bc114495e149c7121ece
Jesus <159750556+thinkaboutj@users.noreply.github.com>
2025-02-13 21:47:07 -0700

    primera parte de la practica al fin terminada

---
+++ PopCornFactory/app/src/main/java/medina/jesus/popcornfactory/MainActivity.kt
@@ -0,0 +1,35 @@
+package medina.jesus.popcornfactory
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.GridView
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val buttonStart: Button = findViewById(R.id.button_get_started)
+
+        buttonStart.setOnClickListener {
+            var intent: Intent = Intent(this, Catalogo::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ PopCornFactory/app/src/main/java/medina/jesus/popcornfactory/Pelicula.kt
@@ -0,0 +1,7 @@
+package medina.jesus.popcornfactory
+
+data class Pelicula(var titulo: String,
+                    var image: Int,
+                    var header: Int,
+                    var sinopsis: String) {
+}
